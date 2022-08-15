c405b522d8dbae07e243f8064b56b72b17b3fa45
Artem Abdullin <artemleelooai@gmail.com>
2022-08-15 23:48:24 +0300

    Added constructor

--- DutchAuction.sol
+++ DutchAuction.sol
@@ -8,3 +8,34 @@ interface IERC721 {
         uint _nftId
     ) external;
 }
+
+contract DutchAuction {
+    uint private constant DURATION = 7 days;
+
+    IERC721 public immutable nft;
+    uint public immutable nftId;
+
+    address payable public immutable seller;
+    uint public immutable startingPrice;
+    uint public immutable startAt;
+    uint public immutable expiresAt;
+    uint public immutable discountRate;
+
+    constructor(
+        uint _startingPrice,
+        uint _discountRate,
+        address _nft,
+        uint _nftId
+    ) {
+        seller = payable(msg.sender);
+        startingPrice = _startingPrice;
+        startAt = block.timestamp;
+        expiresAt = block.timestamp + DURATION;
+        discountRate = _discountRate;
+
+        require(_startingPrice >= _discountRate * DURATION, "starting price < min");
+
+        nft = IERC721(_nft);
+        nftId = _nftId;
+    }
+}
